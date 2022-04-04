028935543b016d2c298e41b9d07798fb998fc503
Madhu <madhupandikkadu69@gmail.com>
2022-04-03 22:34:29 -0700

    bank app login commited

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -13,7 +13,7 @@ export class LoginComponent implements OnInit {
    pwd=""
 
   //creating a data base
-  database={
+  database:any={
     1000:{acno:1000,uname: "neer",password:1000,balance:5000},
     1001:{acno:1001,uname: "neel",password:1001,balance:50000},
     1002:{acno:1002,uname: "neev",password:1002,balance:500000}
@@ -28,17 +28,27 @@ export class LoginComponent implements OnInit {
 
   acNoChange(event:any){
 this.acno=event.target.value
-console.log(event.target.value);
+
 
   }
   
   pwdChange(event:any){
     this.pwd=event.target.value
       }
-
+//login phase
   login() {
+    var acno_l=this.acno;
+    var pwd_l=this.pwd
+    let database=this.database
     
-alert("login click")
+if(acno_l in database){
+
+  if(pwd_l==database[acno_l].password){
+alert("login success")
+  }
+  else{alert("invalid password")}
+}
+else{alert("invalid username")}
   }
 
 }
