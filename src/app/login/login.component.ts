eb06fdc5d40eb8ee98d2323d166d4074c2cbb28e
Madhu <madhupandikkadu69@gmail.com>
2022-04-03 23:10:31 -0700

    one way binding using template reference model

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,17 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-   aim="your banking partner"
-   acnum="account number please!!"
-   acno=""
-   pwd=""
+  aim = "your banking partner"
+  acnum = "account number please!!"
+  acno = ""
+  pwd = ""
 
   //creating a data base
-  database:any={
-    1000:{acno:1000,uname: "neer",password:1000,balance:5000},
-    1001:{acno:1001,uname: "neel",password:1001,balance:50000},
-    1002:{acno:1002,uname: "neev",password:1002,balance:500000}
-  
+  database: any = {
+    1000: { acno: 1000, uname: "neer", password: 1000, balance: 5000 },
+    1001: { acno: 1001, uname: "neel", password: 1001, balance: 50000 },
+    1002: { acno: 1002, uname: "neev", password: 1002, balance: 500000 }
+
   }
 
 
@@ -25,30 +25,48 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
   }
+//login using $event
+//   acNoChange(event: any) {
+//     this.acno = event.target.value
 
-  acNoChange(event:any){
-this.acno=event.target.value
+
+//   }
+
+//   pwdChange(event: any) {
+//     this.pwd = event.target.value
+//   }
+//   //login phase
+//   login() {
+//     var acno_l = this.acno;
+//     var pwd_l = this.pwd
+//     let database = this.database
+
+//     if (acno_l in database) {
+
+//                 if (pwd_l == database[acno_l].password) {
+//                  alert("login success")
+//                }
+//                  else { alert("invalid password") }
+//       }
+//       else { alert("invalid username") }
+// }
+
+//login using template reference variable
 
 
-  }
+login(a:any,p:any) {
+      var acno_l = a.value;
+      var pwd_l = p.value;
+      let database = this.database
   
-  pwdChange(event:any){
-    this.pwd=event.target.value
-      }
-//login phase
-  login() {
-    var acno_l=this.acno;
-    var pwd_l=this.pwd
-    let database=this.database
-    
-if(acno_l in database){
-
-  if(pwd_l==database[acno_l].password){
-alert("login success")
-  }
-  else{alert("invalid password")}
-}
-else{alert("invalid username")}
+      if (acno_l in database) {
+  
+                  if (pwd_l == database[acno_l].password) {
+                   alert("login success")
+                 }
+                   else { alert("invalid password") }
+        }
+        else { alert("invalid username") }
   }
 
 }
